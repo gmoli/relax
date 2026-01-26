ba5514997a50211167b2b74c0c7f27f2aa70a5f7
gmoli <gabrukas@gmail.com>
2026-01-26 18:49:22 -0300

    Update groupUtils.ts

--- src/calc2/utils/groupUtils.ts
+++ src/calc2/utils/groupUtils.ts
@@ -9,11 +9,13 @@ import { parseRelalgGroup, relalgFromRelalgAstNode, replaceVariables } from 'db/
 import * as jQuery from 'jquery';
 import {string} from "prop-types";
 
+const ld_ejercicio51: any = require('../data/ejercicio_5_1.txt');
 const ld_sb: any = require('../data/sb.txt');
 const ld_ufes: any = require('../data/ufes.txt');
 const ld: any = require('../data/misc.txt');
 const ld_uibk: any = require('../data/uibk.txt');
 const LOCAL_DATA: { [id: string]: string } = {
+	'ejercicio51': ld_ejercicio51.default ? ld_ejercicio51.default : '',
 	'sb': ld_sb.default ? ld_sb.default : '',
   'ufes': ld_ufes.default ? ld_ufes.default : '',
   'misc': ld.default ? ld.default : '',
